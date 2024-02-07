bb8f840954b1788ca5f29254e2332391a5d34be5
Alessandro Sylar <lsn.lodigiani@gmail.com>
2024-02-07 12:45:39 +0100

    update template

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,15 @@
 <template>
-
-  <NavBar/>
-
-  <h1 class="text-danger text-uppercase">Ciao Mondo</h1>
-
-  <hr>
-
-  <CompSaluto/>
-
-  <hr>
-
-  <CompTitoloColore/>
-
 </template>
 
 
 <script>
-import CompSaluto from './components/CompSaluto.vue'
-import CompTitoloColore from './components/CompTitoloColore.vue'
-import NavBar from './components/header/NavBar.vue'
-
 
   export default{
-    components: {
-      CompSaluto,
-      CompTitoloColore,
-      NavBar
-    }
+
   }
 </script>
 
 
-<style>
-  h1{
-    color: red;
-  }
+<style lang="scss">
+
 </style>
